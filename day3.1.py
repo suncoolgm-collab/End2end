3c9322c7cc87b8b322bc5f7199968ea22f697b8a
suncoolgm-collab <suncoolgm@gmail.com>
2026-09-05 14:34:25 +0900

    Phase2 실습2

--- day3.1.py
+++ day3.1.py
@@ -1,30 +1,40 @@
 import socket
+import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
-s.bind(('127.0.0.1', 8000))                                     
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 8000))
 s.listen()
-while True:
-    conn, address = s.accept()   
+
+def handle(conn, address):
+    conn.settimeout(2)
     buffer = b""
     while True:
-        chunk= conn.recv(1024)
-        if chunk==b"":
+        try:
+            chunk = conn.recv(1024)
+        except socket.timeout:
             break
-        
-        buffer+=chunk
-        
+        if chunk == b"":
+            break
+
+        buffer += chunk
+
         if b"\r\n\r\n" in buffer:
             break
-    
+
     if b"\r\n\r\n" not in buffer:
         conn.close()
-        continue        
-    print(buffer.decode())   
-    respd=(
+        return
+    print(buffer.decode())
+    respd = (
         "HTTP/1.1 200 OK\r\n"
         "Content-Type: text/plain; charset=utf-8\r\n"
-        "Content-Length: 5\r\n\r\n"
+        "Content-Length: 5\r\n"
+        "Connection: close\r\n\r\n"
         "hello"
         )
     conn.sendall(respd.encode())
     conn.close()
+
+while True:
+    conn, address = s.accept()
+    threading.Thread(target=handle, args=(conn, address)).start()
